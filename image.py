66aa11fb30656ba02f67e8264ed3d3064e5389d7
mikhailm <shefferger@gmail.com>
2023-07-28 21:20:42 +0300

    Created text alignment, final

--- image.py
+++ image.py
@@ -1,9 +1,13 @@
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw, ImageFont
+import textwrap
 import io
 
 
 def make_demotivator(img: bytes, text: str):
     pimg = Image.open(fp=io.BytesIO(img))
+    for i in pimg.size:
+        if i < 100:
+            return None
     match pimg.mode:
         case 'RGB':
             file_type = 'JPEG'
@@ -15,8 +19,20 @@ def make_demotivator(img: bytes, text: str):
     pimg = pimg.resize(size=(min_size, min_size))
     pimg = ImageOps.expand(pimg, border=2, fill='black')
     pimg = ImageOps.expand(pimg, border=2, fill='white')
-    borders = (round(min_size / 20), round(min_size / 20), round(min_size / 20), round(min_size / 4))
+    borders = (round(min_size / 20), round(min_size / 20), round(min_size / 20), round(min_size / 20))
     pimg = ImageOps.expand(pimg, border=borders, fill='black')
+    font_size = round(min_size / 10)
+    font = ImageFont.truetype('timesnewroman.ttf', font_size)
+    current_h, pad = pimg.height - round(min_size / 21), 2
+    para = textwrap.wrap(text=text, width=23)
+    for line in para:
+        draw = ImageDraw.Draw(pimg)
+        w, h = draw.textsize(text=line, font=font)
+        borders = (0, 0, 0, h)
+        pimg = ImageOps.expand(pimg, border=borders, fill='black')
+        draw = ImageDraw.Draw(pimg)
+        draw.text(((pimg.width - w) / 2, current_h), line, font=font, fill=(255, 255, 255))
+        current_h += h + pad
     buffer = io.BytesIO()
     pimg.save(fp=buffer, format=file_type)
     return buffer.getvalue()
@@ -25,7 +41,9 @@ def make_demotivator(img: bytes, text: str):
 def test():
     with open(mode='rb', file='samples/test.png') as fd:
         file = fd.read()
-        make_demotivator(file)
+        img = make_demotivator(img=file, text='hello world! yay! its a good day today, isnt it? i really like it ')
+        with open(mode='wb', file='/mnt/c/Users/sheff/Desktop/res/sample-out.png') as fd2:
+            fd2.write(img)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -26,15 +26,22 @@ class App:
             task.cancel()
 
     def register_handlers(self):
-        @self.bot.message_handler(commands=['help', 'start', 'status'])
+        @self.bot.message_handler(commands=['help', 'start'])
         async def start_menu_handler(msg):
             cid = msg.chat.id
-            await self.bot.send_message(chat_id=cid, text='Text', parse_mode='HTML')
+            text = 'Привет!\nДля создания демотиватора пришли фотографию с ' \
+                   'текстом одним сообщением.\n\nИзображение должно быть не меньше 100x100px и не больше 10MB. Текст ' \
+                   'должен быть не больше 200 символов. Принимаются изображения формата png, jpeg, gif. Если картинке ' \
+                   'необходимо кадрирование - воспользуйся встроенным в телеграм редактором.'
+            await self.bot.send_message(chat_id=cid, text=text, parse_mode='HTML')
 
         @self.bot.message_handler(content_types=['photo', 'document'])
         async def photo_handler(msg):
             if msg.caption is None:
-                await self.bot.reply_to(message=msg, text='Вы забыли написать текст, попробуйте еще раз')
+                await self.bot.reply_to(message=msg, text='Вы не написали текст, попробуйте еще раз')
+                return
+            elif len(msg.caption) > 200:
+                await self.bot.reply_to(message=msg, text='Вы указали слишком длинный текст (не больше 200 символов)')
                 return
             file_id, img_result = None, None
             if msg.content_type == 'photo' and msg.caption is not None:
@@ -43,6 +50,7 @@ class App:
                 if msg.document.mime_type in ('image/png', 'image/pjpeg', 'image/jpeg', 'image/gif'):
                     file_id = msg.document.file_id
             cid = msg.chat.id
+            print(f'cid: {cid}, text: {msg.caption}')
             if file_id:
                 file_info = await self.bot.get_file(file_id)
                 img = await self.bot.download_file(file_info.file_path)
